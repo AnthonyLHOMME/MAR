9c700ca2c50d21d65e5085127fa392681d7c347a
Anthony LHOMME <lhomme.anthony@gmail.com>
2015-01-16 15:34:44 +0100

    [v2] camera (replay)

--- projetV2/main.2.js
+++ projetV2/main.2.js
@@ -7,11 +7,13 @@ requirejs(['ModulesLoaderV2.js'], function()
 		{ 
 			// Level 0 includes
 			ModulesLoader.requireModules(["../threejs/three.min.js"]) ;
-			ModulesLoader.requireModules([ "myJS/ThreeRenderingEnv.js", 
-			                              "myJS/ThreeLightingEnv.js", 
-			                              "myJS/ThreeLoadingEnv.js", 
-			                              "myJS/navZ.js",
-			                              "FlyingVehicle.js"]) ;
+			ModulesLoader.requireModules([
+				"myJS/ThreeRenderingEnv.js", 
+				"myJS/ThreeLightingEnv.js",
+				"myJS/ThreeLoadingEnv.js",
+				"myJS/navZ.js",
+				"FlyingVehicle.js"
+			]) ;
 			// Loads modules contained in includes and starts main function
 			ModulesLoader.loadModules(start) ;
 		}
@@ -28,11 +30,13 @@ function start(){
 	//	---------------------------------------------------------------------------- 			
 	//	global vars
 	//	----------------------------------------------------------------------------
+	var isModeCine = false;
 
 	// Comptage des tours
-	var targetPane = 0;
+	var targetPlane = 1;
 	var numTour = 0;
-
+	var maxTour = 3;
+	
 	//	keyPressed
 	var currentlyPressedKeys = {};
 	
@@ -46,12 +50,10 @@ function start(){
 	var dx = 1.0;
 
 	// Creates the vehicle (handled by physics)
-	var vehicle = new FlyingVehicle(
-			{
-				position: new THREE.Vector3(CARx, CARy, CARz),
-				zAngle : CARtheta+Math.PI/2.0,
-			}
-			) ;
+	var vehicle = new FlyingVehicle({
+		position: new THREE.Vector3(CARx, CARy, CARz),
+		zAngle : CARtheta+Math.PI/2.0,
+	}) ;
 	
 	//	rendering env
 	var RC =  new ThreeRenderingEnv();
@@ -92,11 +94,11 @@ function start(){
 	var car3 = Loader.load({filename: 'assets/car_Zup_01.obj', node: car2, name: 'car3'}) ;
 	car3.position.z= +0.25 ;
 	// attach the scene camera to car
-	car3.add(RC.camera) ;
-	RC.camera.position.x = 0.0 ;
-	RC.camera.position.z = 10.0 ;
-	RC.camera.position.y = -25.0 ;
-	RC.camera.rotation.x = 85.0*3.14159/180.0 ;
+	car3.add(RC.camera[0]) ;
+	RC.camera[0].position.x = 0.0 ;
+	RC.camera[0].position.z = 10.0 ;
+	RC.camera[0].position.y = -25.0 ;
+	RC.camera[0].rotation.x = 85.0*3.14159/180.0 ;
 		
 	//	Skybox
 	Loader.loadSkyBox('assets/maps',['px','nx','py','ny','pz','nz'],'jpg', RC.scene, 'sky',4000);
@@ -104,12 +106,12 @@ function start(){
 	//	Planes Set for Navigation 
 	// 	z up 
 	var NAV = new navPlaneSet(
-					new navPlane('p01',	-260, -180,	 -80, 120,	+0,+0,'px')); 		// 01	
-	NAV.addPlane(	new navPlane('p02', -260, -180,	 120, 200,	+0,+20,'py')); 		// 02		
+					new navPlane('p01',	-260, -180,	 -80, 120,	+00,+00,'px')); 	// 01	
+	NAV.addPlane(	new navPlane('p02', -260, -180,	 120, 200,	+00,+20,'py')); 	// 02		
 	NAV.addPlane(	new navPlane('p03', -260, -240,	 200, 240,	+20,+20,'px')); 	// 03		
 	NAV.addPlane(	new navPlane('p04', -240, -160,  200, 260,	+20,+20,'px')); 	// 04		
 	NAV.addPlane(	new navPlane('p05', -160,  -80,  200, 260,	+20,+40,'px')); 	// 05		
-	NAV.addPlane(	new navPlane('p06',  -80, -20,   200, 260,	+40,+60,'px')); 	// 06		
+	NAV.addPlane(	new navPlane('p06',  -80,  -20,  200, 260,	+40,+60,'px')); 	// 06		
 	NAV.addPlane(	new navPlane('p07',  -20,  +40,  140, 260,	+60,+60,'px')); 	// 07		
 	NAV.addPlane(	new navPlane('p08',    0,  +80,  100, 140,	+60,+60,'px')); 	// 08		
 	NAV.addPlane(	new navPlane('p09',   20, +100,   60, 100,	+60,+60,'px')); 	// 09		
@@ -117,8 +119,8 @@ function start(){
 	NAV.addPlane(	new navPlane('p11',  100,  180,   40, 100,	+40,+60,'nx')); 	// 11		
 	NAV.addPlane(	new navPlane('p12',  180,  240,   40,  80,	+40,+40,'px')); 	// 12		
 	NAV.addPlane(	new navPlane('p13',  180,  240,    0,  40,	+20,+40,'py')); 	// 13 		
-	NAV.addPlane(	new navPlane('p14',  200,  260,  -80,   0,	+0,+20,'py')); 		// 14		
-	NAV.addPlane(	new navPlane('p15',  180,  240, -160, -80,	+0,+40,'ny')); 		// 15		
+	NAV.addPlane(	new navPlane('p14',  200,  260,  -80,   0,	+00,+20,'py')); 	// 14		
+	NAV.addPlane(	new navPlane('p15',  180,  240, -160, -80,	+00,+40,'ny')); 	// 15		
 	NAV.addPlane(	new navPlane('p16',  160,  220, -220,-160,	+40,+40,'px')); 	// 16	
 	NAV.addPlane(	new navPlane('p17',   80,  160, -240,-180,	+40,+40,'px')); 	// 17	
 	NAV.addPlane(	new navPlane('p18',   20,   80, -220,-180,	+40,+40,'px')); 	// 18	
@@ -136,22 +138,35 @@ function start(){
 	NAV.addPlane(	new navPlane('p30', -240, -180, -140, -80,	+0,+20,'ny')); 		// 30			
 	NAV.setPos(CARx,CARy,CARz); 
 	NAV.initActive();
+	
 	// DEBUG
 	//NAV.debug();
 	//var navMesh = NAV.toMesh();
 	//RC.addToScene(navMesh);
+	
+	//	---------------------------------------------------------------------------
 	//	event listener
 	//	---------------------------------------------------------------------------
 	//	resize window
 	window.addEventListener( 'resize', onWindowResize, false );
+	
 	//	keyboard callbacks 
 	document.onkeydown = handleKeyDown;
-	document.onkeyup = handleKeyUp;					
+	document.onkeyup = handleKeyUp;
+	//	---------------------------------------------------------------------------
 
+
+	//	---------------------------------------------------------------------------
 	//	callback functions
 	//	---------------------------------------------------------------------------
 	function handleKeyDown(event) { currentlyPressedKeys[event.keyCode] = true;}
-	function handleKeyUp(event) {currentlyPressedKeys[event.keyCode] = false;}
+	function handleKeyUp(event) {
+		if (currentlyPressedKeys[80]) {
+			// (P) Change camera
+			isModeCine = !isModeCine;
+		}
+		currentlyPressedKeys[event.keyCode] = false;
+	}
 
 	function handleKeys() {
 		if (currentlyPressedKeys[67]) // (C) debug
@@ -181,10 +196,12 @@ function start(){
 
 	//	window resize
 	function  onWindowResize() {RC.onWindowResize(window.innerWidth,window.innerHeight);}
+	//	---------------------------------------------------------------------------
 
 	function render() { 
 		requestAnimationFrame( render );
 		handleKeys();
+		
 		// Vehicle stabilization 
 		vehicle.stabilizeSkid(50) ; 
 		vehicle.stabilizeTurn(1000) ;
@@ -194,19 +211,106 @@ function start(){
 		newPosition.sub(oldPosition) ;
 		// NAV
 		NAV.move(newPosition.x, newPosition.y, 150,10) ;
-		// car0
-		car0.position.set(NAV.x, NAV.y, NAV.z) ;
+		
 		// Updates the vehicle
 		vehicle.position.x = NAV.x ;
 		vehicle.position.y = NAV.Y ;
+		// Updates car0
+		car0.position.set(NAV.x, NAV.y, NAV.z) ;
 		// Updates car1
 		car1.matrixAutoUpdate = false;		
 		car1.matrix.copy(NAV.localMatrix(CARx,CARy));
 		// Updates car2
 		car2.rotation.z = vehicle.angles.z-Math.PI/2.0 ;
-		// Rendering
-		RC.renderer.render(RC.scene, RC.camera); 
+		
+		var activePlane = NAV.findActive(NAV.x, NAV.y);
+		if (activePlane == targetPlane) {
+			targetPlane++;
+			if (activePlane == 1) {
+				numTour++;
+				document.getElementById("counter").innerHTML = numTour+" / "+maxTour;
+			}
+			if (targetPlane == 30) { // raz
+				targetPlane = 0;
+			}
+			// cas patriculier (blocs facultatif)
+			if (targetPlane == 2 || targetPlane == 9 || targetPlane == 20 || targetPlane == 22 || targetPlane == 25 || targetPlane == 27) {
+				targetPlane++;
+			}
+		}
+		
+		document.getElementById("block").innerHTML = activePlane;
+			
+		var carPos = new THREE.Vector3(NAV.x, NAV.y, NAV.z);
+		renderingCamera(activePlane, carPos);
 	};
+	
+	function renderingCamera(activePlane, carPos) {
+		// Parametrage des cameras
+		if (isModeCine) {
+			// mode cinematique
+			switch (activePlane) {
+				case "0":
+					RC.renderer.render(RC.scene, RC.camera[1]);
+					break;
+				case "1":
+				case "2":
+				case "3":
+				case "4":
+					RC.camera[2].up = new THREE.Vector3(0,0,1);
+					RC.camera[2].lookAt(carPos);
+					RC.renderer.render(RC.scene, RC.camera[2]);
+					break;
+				case "5":
+				case "6":
+				case "7":
+					RC.renderer.render(RC.scene, RC.camera[3]);
+					break;
+				case "8":
+				case "9":
+				case "10":
+				case "11":
+					RC.renderer.render(RC.scene, RC.camera[4]);
+					break;
+				case "12":
+				case "13":
+				case "14":
+					RC.camera[5].up = new THREE.Vector3(0,0,1);
+					RC.camera[5].lookAt(carPos);
+					RC.renderer.render(RC.scene, RC.camera[5]);
+					break;
+				case "15":
+				case "16":
+				case "17":
+				case "18":
+				case "19":
+					RC.renderer.render(RC.scene, RC.camera[6]);
+					break;
+				case "20":
+				case "21":
+				case "22":
+					RC.camera[7].up = new THREE.Vector3(0,0,1);
+					RC.camera[7].lookAt(carPos);
+					RC.renderer.render(RC.scene, RC.camera[7]);
+					break;
+				case "23":
+				case "24":
+				case "25":
+				case "26":
+				case "27":
+				case "28":
+				case "29":
+					RC.renderer.render(RC.scene, RC.camera[8]);
+					break;
+				default:
+					RC.renderer.render(RC.scene, RC.camera[0]);
+					break;
+			}
+		} else {
+			// mode third person
+			RC.renderer.render(RC.scene, RC.camera[0]); 
+		}
+	}
 
 	render(); 
 }
--- projetV2/myJS/ThreeRenderingEnv.js
+++ projetV2/myJS/ThreeRenderingEnv.js
@@ -13,8 +13,34 @@ function ThreeRenderingEnv(){
 	//this.scene.fog =  new THREE.FogExp2( 0x5876A4, 0.003 );
 
 	// camera
-	this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.01, 5000 );
-
+	this.camera = [];
+	this.camera.push(new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 5000)); // camera TPS
+	this.camera.push(new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 5000));
+	this.camera[1].position.set(-260, 140, 5);
+	this.camera[1].rotation.x = 1.57;
+	this.camera[1].rotation.y = -2.57;
+	this.camera.push(new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 5000));
+	this.camera[2].position.set(-175, 195, 40);
+	this.camera.push(new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 5000));
+	this.camera[3].position.set(50, 270, 90);
+	this.camera[3].rotation.x = 1.8;
+	this.camera[3].rotation.y = 2.5;
+	this.camera.push(new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 5000));
+	this.camera[4].position.set(-20, 100, 100);
+	this.camera[4].rotation.x = 2;
+	this.camera[4].rotation.y = -1.8;
+	this.camera[4].rotation.z = 0.5;
+	this.camera.push(new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 5000));
+	this.camera[5].position.set(260, -80, 5);
+	this.camera.push(new THREE.PerspectiveCamera(100, window.innerWidth/window.innerHeight, 0.1, 5000));
+	this.camera[6].position.set(120, -250, 50);
+	this.camera[6].rotation.x = 1.57;
+	this.camera.push(new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 5000));
+	this.camera[7].position.set(-90, -40, 100);
+	this.camera.push(new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 5000));
+	this.camera[8].position.set(-155, -280, 90);
+	this.camera[8].rotation.x = 1;
+	
 	// renderer
 	this.renderer = new THREE.WebGLRenderer(); 
 	// default background color
@@ -38,8 +64,10 @@ function ThreeRenderingEnv(){
 	
 	// onWindoResize
 	this.onWindowResize = function(w,h) {
-		this.camera.aspect = w / h;
-		this.camera.updateProjectionMatrix();
+		for(c of this.camera) {
+			c.aspect = w / h;
+			c.updateProjectionMatrix();
+		}
 		this.renderer.setSize( w, h );
 	}
 }
